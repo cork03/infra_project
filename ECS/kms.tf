9ffdb3f068496267fb6aa0fa4e5582cf7992a964
住吉翼 <tsubasa.t1119@gmail.com>
2023-03-21 23:09:14 +0900

    不要リソース削除
    kmsリソース削除

--- ECS/kms.tf
+++ ECS/kms.tf
@@ -1,13 +1,13 @@
-resource "aws_kms_key" "example" {
-  description = "example kms key"
-  # キーの変更を有効化
-  enable_key_rotation = true
-  is_enabled          = true
-  # 削除待機期間
-  deletion_window_in_days = 30
-}
+# resource "aws_kms_key" "example" {
+#   description = "example kms key"
+#   # キーの変更を有効化
+#   enable_key_rotation = true
+#   is_enabled          = true
+#   # 削除待機期間
+#   deletion_window_in_days = 30
+# }
 
-resource "aws_kms_alias" "example-alias" {
-  name          = "alias/example"
-  target_key_id = aws_kms_key.example.id
-}
+# resource "aws_kms_alias" "example-alias" {
+#   name          = "alias/example"
+#   target_key_id = aws_kms_key.example.id
+# }
